d743134070f584f3c617de2b91a665bec4e47141
ivan <42503922+iesful@users.noreply.github.com>
2022-11-07 21:49:19 -0600

    d

--- Database Project/main.py
+++ Database Project/main.py
@@ -8,22 +8,20 @@ dpg.create_context()
 dpg.create_viewport(title='Custom Title', width=800, height=1000)
 dpg.setup_dearpygui()
 
-#testing
-print('test')
 
-with dpg.window(label="Example Window"):
-    dpg.add_text("Hello world")
+with dpg.window(label="C^2"):
+    dpg.add_text("Examinee Form")
     dpg.add_button(label="Save", callback=save_callback)
-    dpg.add_input_text(label="string")
-    dpg.add_slider_float(label="float")
+    dpg.add_input_text(label="string", default_value='First Name')
+    #dpg.add_slider_float(label="float")
 
 dpg.show_viewport()
-#dpg.start_dearpygui()
 
+#can replace start_dearpygui()
 while dpg.is_dearpygui_running():
-    print('running')
+    #print('running')
     dpg.render_dearpygui_frame()
 
-print('eneded')
+print('ended')
 
 dpg.destroy_context()
