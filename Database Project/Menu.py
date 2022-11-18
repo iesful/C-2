2c56cc1fa7a2b0f6f121ae5e57227c029ed8c67d
TimothySz <116852547+TimothySz@users.noreply.github.com>
2022-11-18 15:57:27 -0600

    Created a new python file for the menu

---
+++ Database Project/Menu.py
@@ -0,0 +1,40 @@
+#text based menu to connect and interact with our database
+import sqlite3
+connection = sqlite3.connect("C^2.db")
+crusor = connection.cursor
+
+print ("\
+C^2 Database Menu\n\n\
+1.Create a new Table\n\
+2.Drop a table\n\
+3.Alter a table\n\
+4.Update a table\n\
+5.Insert into a table\n\
+6.Delete from a table\n\
+7.Search for a record in a table"
+)
+#takes the user's choice from the above options
+ActionChoice = input("Please type the number infront of the action you would like to take...}")
+
+if ActionChoice == 1:
+    cancelChoice = input("You have selected to create a new table would you like to continue? Y/N")
+    if cancelChoice.upper == "Y":
+        #the name you want to give the table 
+        userTableName = input("Please enter the name you would like to give the new table...")
+        #the fields you want to include in the table
+        newTableFieldName = input("Please input the names of the fields you want in your table separated by commas...")
+        #sql statment to add the table to the database goes here
+    else:
+        ActionChoice = input("Please type the number infront of the action you would like to take...}")
+
+
+if ActionChoice == 2:
+    cancelChoice = input("You have selected to drop a table would you like to continue? Y/N")
+    if cancelChoice.upper == "Y":
+        #the name of the table you would like to drop from the database
+        userTableDropChoice = input("Please enter the name of the table you would like to drop...")
+        #sql to drop the table from the database goes here
+    else:
+        ActionChoice = input("Please type the number infront of the action you would like to take...}")
+
+#continue the if statements until all menu options are covered
