dba25fbcf9dbe0dc87efb8f6d1f595b11e45e43a
ivan <42503922+iesful@users.noreply.github.com>
2022-11-20 11:04:50 -0600

    changed some wording and other small changes

--- Database Project/Menu.py
+++ Database Project/Menu.py
@@ -1,7 +1,7 @@
 #text based menu to connect and interact with our database
 import sqlite3
 connection = sqlite3.connect("C^2.db")
-crusor = connection.cursor
+cursor = connection.cursor
 
 print ("\
 | C^2 Database Menu |\n\n\
@@ -21,8 +21,8 @@ leave = 'N'
 while leave != "Y":
 
     if ActionChoice == 1:
-        cancelChoice = input("You have selected to create a new table would you like to continue (Y/N)? ")
-        if cancelChoice.upper == "Y":
+        prompt = input("You have selected to create a new table would you like to continue (Y/N)? ")
+        if prompt.upper == "Y":
             #the name you want to give the table 
             userTableName = input("Please enter the name you would like to give the new table: ")
             #the fields you want to include in the table
@@ -32,8 +32,8 @@ while leave != "Y":
             ActionChoice = input("Please type the number infront of the action you would like to take: ")
 
     if ActionChoice == 2:
-        cancelChoice = input("You have selected to drop a table would you like to continue (Y/N)? ")
-        if cancelChoice.upper == "Y":
+        prompt = input("You have selected to drop a table would you like to continue (Y/N)? ")
+        if prompt.upper == "Y":
             #the name of the table you would like to drop from the database
             userTableDropChoice = input("Please enter the name of the table you would like to drop: ")
             #sql to drop the table from the database goes here
@@ -42,9 +42,9 @@ while leave != "Y":
 
     #option 9 logic
     if ActionChoice == '9':
-        cancelChoice = input("You have selected to exit the program would you like to continue (Y/N)? ").upper()
-        if cancelChoice == "Y":
-            leave = cancelChoice
+        prompt = input("You have selected to exit the program would you like to continue (Y/N)? ").upper()
+        if prompt == "Y":
+            leave = prompt
             print("\n| Exiting the C^2 Database Menu |")
         else:
             ActionChoice = input("Please type the number infront of the action you would like to take: ")
