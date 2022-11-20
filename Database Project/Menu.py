74f00e071f7ae96347a61d896fa8d3975601d073
ivan <42503922+iesful@users.noreply.github.com>
2022-11-20 10:12:24 -0600

    changes to #9 to get it to work & other changes

--- Database Project/Menu.py
+++ Database Project/Menu.py
@@ -4,49 +4,50 @@ connection = sqlite3.connect("C^2.db")
 crusor = connection.cursor
 
 print ("\
-C^2 Database Menu\n\n\
-1.Create a new Table\n\
-2.Drop a table\n\
-3.Alter a table\n\
-4.Update a table\n\
-5.Insert into a table\n\
-6.Delete from a table\n\
-7.Search for a record in a table\n\
-8.View reports\n\
-9.Quit")
+| C^2 Database Menu |\n\n\
+1. Create a new Table\n\
+2. Drop a table\n\
+3. Alter a table\n\
+4. Update a table\n\
+5. Insert into a table\n\
+6. Delete from a table\n\
+7. Search for a record in a table\n\
+8. View reports\n\
+9. Quit\n")
 
-i = "N"
-while i != "Y":
-    #takes the user's choice from the above options
-    ActionChoice = input("Please type the number infront of the action you would like to take...}")
+#takes the user's choice from the above options
+ActionChoice = input("Please type the number infront of the action you would like to take: ")
+leave = 'N'
+while leave != "Y":
 
     if ActionChoice == 1:
-        cancelChoice = input("You have selected to create a new table would you like to continue? Y/N")
+        cancelChoice = input("You have selected to create a new table would you like to continue (Y/N)? ")
         if cancelChoice.upper == "Y":
             #the name you want to give the table 
-            userTableName = input("Please enter the name you would like to give the new table...")
+            userTableName = input("Please enter the name you would like to give the new table: ")
             #the fields you want to include in the table
-            newTableFieldName = input("Please input the names of the fields you want in your table separated by commas...")
+            newTableFieldName = input("Please input the names of the fields you want in your table separated by commas: ")
             #sql statment to add the table to the database goes here
         else:
-            ActionChoice = input("Please type the number infront of the action you would like to take...}")
-
+            ActionChoice = input("Please type the number infront of the action you would like to take: ")
 
     if ActionChoice == 2:
-        cancelChoice = input("You have selected to drop a table would you like to continue? Y/N")
+        cancelChoice = input("You have selected to drop a table would you like to continue (Y/N)? ")
         if cancelChoice.upper == "Y":
             #the name of the table you would like to drop from the database
-            userTableDropChoice = input("Please enter the name of the table you would like to drop...")
+            userTableDropChoice = input("Please enter the name of the table you would like to drop: ")
             #sql to drop the table from the database goes here
         else:
-            ActionChoice = input("Please type the number infront of the action you would like to take...}")
-    if ActionChoice == 9:
-        cancelChoice = input("You have selected to exit the program would you like to continue? Y/N")
-        if cancelChoice.upper == "Y":
-            print("Goodbye")
-            i = "Y"
+            ActionChoice = input("Please type the number infront of the action you would like to take: ")
+
+    #option 9 logic
+    if ActionChoice == '9':
+        cancelChoice = input("You have selected to exit the program would you like to continue (Y/N)? ").upper()
+        if cancelChoice == "Y":
+            leave = cancelChoice
+            print("\n| Exiting the C^2 Database Menu |")
         else:
-            ActionChoice = input("Please type the number infront of the action you would like to take...}")
+            ActionChoice = input("Please type the number infront of the action you would like to take: ")
 
 
     #continue the if statements until all menu options are covered
